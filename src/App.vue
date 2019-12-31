a400716abef70335f6ba7a0ef7de3fdff4845431
Michael Song <michael.song.hz@gmail.com>
2019-12-30 23:14:49 -0500

    Improved navbar on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
-    <div class="navbar">
-      <h1><b>Dr Q. Song's Homepage</b></h1>
-      <div class="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/albums">Albums</router-link> |
-        <router-link to="/about">About</router-link>
+    <div class="container">
+      <div class="navbar d-flex">
+        <div class="navbar__header pt-1">
+          <h1><b>Dr Q. Song's Homepage</b></h1>
+        </div>
+        <!-- <div class="navbar__nav align-self-end"> -->
+        <div class="navbar__nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/albums">Albums</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
       </div>
     </div>
     <router-view />
