02f9c6e907b38fc033df7ec46fa63430c815a9b3
Michael Song <michael.song.hz@gmail.com>
2020-10-29 01:50:47 -0400

    Updated about info

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,17 +3,12 @@
     <section class="jumbotron text-center">
       <div class="container">
         <h1 class="jumbotron-heading">About</h1>
-        <!-- <p class="lead text-muted">
-        </p> -->
       </div>
     </section>
 
     <div class="container">
-      <h3>Some fonts on this page use Font Awesome Icons</h3>
-      <p>Copyright (C) 2016 by Dave Gandy</p>
-      <p>Author: Dave Gandy</p>
-      <p>License: SIL</p>
-      <p>Homepage: http://fortawesome.github.com/Font-Awesome/</p>
+      <p>Showcasing bird photography from around the world, by Dr Q Song.</p>
+      <p>All photographs featured on this page are original work.</p>
     </div>
   </main>
 </template>
